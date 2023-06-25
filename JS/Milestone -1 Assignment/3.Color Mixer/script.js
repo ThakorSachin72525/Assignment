c49113b27388c01c0c275a786970ca87a41e071b
Sachin Thakor <sachinthakor722446@gmail.com>
2023-06-25 06:05:43 +0530

    3.Color Mixer

--- JS/Milestone -1 Assignment/3.Color Mixer/script.js
+++ JS/Milestone -1 Assignment/3.Color Mixer/script.js
@@ -11,8 +11,25 @@ let result = document.getElementById('result');
 let mix = document.getElementById('mix');
 
 mix.addEventListener('click', () => {
-    if (red.checked) {
-        result.innerHTML = 'red'
-    } else
-    result.innerHTML = 'green'
+    if (red.checked && green.checked) {
+        result.innerHTML = 'red & green';
+        result.style.backgroundColor = '#804000'
+    } else if (red.checked && yellow.checked) {
+        result.innerHTML ='red & yellow';
+        result.style.backgroundColor = '#ff8000'
+    } else if (red.checked && blue.checked) {
+        result.innerHTML ='red & blue';
+        result.style.backgroundColor = '#800080'
+    }
+      else if (green.checked && yellow.checked) {
+        result.innerHTML = 'green & yellow';
+        result.style.backgroundColor = '#80c000'
+    } else if (green.checked && blue.checked) {
+        result.innerHTML = 'green & blue';
+        result.style.backgroundColor = '#004080'
+    } 
+      else if (yellow.checked && blue.checked) {
+        result.innerHTML = 'yellow & blue';
+        result.style.backgroundColor = '#808080'
+    }
 });
