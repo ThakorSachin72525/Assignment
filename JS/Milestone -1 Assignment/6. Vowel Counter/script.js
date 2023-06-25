1cc31fdf701128507dc29c1f9a946d6c7da9eca9
Sachin Thakor <sachinthakor722446@gmail.com>
2023-06-26 01:21:37 +0530

    V1

---
+++ JS/Milestone -1 Assignment/6. Vowel Counter/script.js
@@ -0,0 +1,8 @@
+let vowel = ["a", "e", "i", "o", "u"]
+
+// let input = document.getElementById("input");
+let input = ["a", "b", "c", "d", "e"]
+let btn = document.getElementById("btn");
+let result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
