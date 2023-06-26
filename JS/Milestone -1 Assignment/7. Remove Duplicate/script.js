4dfc35df950d2fd15c607918bfb7fce34d6487b7
Sachin Thakor <sachinthakor722446@gmail.com>
2023-06-27 00:28:06 +0530

    7. Remove Duplicate Upload

--- JS/Milestone -1 Assignment/7. Remove Duplicate/script.js
+++ JS/Milestone -1 Assignment/7. Remove Duplicate/script.js
@@ -5,3 +5,18 @@ let i4 = document.getElementById('i4');
 let btn = document.getElementById('btn');
 let item_name = document.getElementById('item_name');
 
+let a = [];
+
+function addItem(i1, i2, i3, i4) {
+  return a.push(i1, i2, i3, i4);
+}
+
+function addItems(i1, i2, i3, i4) {
+  a = [i1, i2, i3, i4];
+  a = a.filter((item, index) => a.indexOf(item) === index);
+}
+
+btn.addEventListener('click', () => {
+  addItems(i1.value, i2.value, i3.value, i4.value);
+  item_name.innerHTML = a.join(', ');
+});
