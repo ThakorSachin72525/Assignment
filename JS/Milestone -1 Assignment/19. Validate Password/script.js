3556c5893f198432d40396a47fd691b20bbfb914
Sachin Thakor <sachinthakor722446@gmail.com>
2023-06-28 23:43:38 +0530

    V1

---
+++ JS/Milestone -1 Assignment/19. Validate Password/script.js
@@ -0,0 +1,9 @@
+
+btn.addEventListener('click', () => {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let btn = document.getElementById('btn');
+    let result = document.getElementById('result');
+    
+    
+})
