c9ac6ea13df383a73163440a6b07cd81da6a469d
Sachin Thakor <sachinthakor722446@gmail.com>
2023-06-29 03:48:09 +0530

    19. Validate Password uploaded

--- JS/Milestone -1 Assignment/19. Validate Password/script.js
+++ JS/Milestone -1 Assignment/19. Validate Password/script.js
@@ -4,6 +4,15 @@ btn.addEventListener('click', () => {
     let password = document.getElementById('password').value;
     let btn = document.getElementById('btn');
     let result = document.getElementById('result');
-    
+    if (email.includes('@') && password.length > 8){
+        result.innerHTML = "Valid Email and Password"
+        result.style.color = "green";
+        result.style.backgroundColor = "black";
+        
+    }else {
+        result.innerHTML = "Invalid Email and Password"
+        result.style.color = "red";
+        result.style.backgroundColor = "black";
+        }
     
 })
