9feb944c5610370e315e47262bb3de0090da4036
Sachin Thakor <sachinthakor722446@gmail.com>
2023-07-11 00:12:19 +0530

    1. Type Conversion Upload

--- JS/Error Handling and Prototypes Assignment - 13/1. Type Conversion/script.js
+++ JS/Error Handling and Prototypes Assignment - 13/1. Type Conversion/script.js
@@ -1,2 +1,15 @@
-let a = "Hello"
-console.log(a);
+function convertToNumber(str) {
+    try {
+        const num = Number(str);
+        if (isNaN(num)) {
+            throw new Error("Invalid number");
+        }
+        return num;
+    } catch (error) {
+        return "Invalid number";
+    }
+}
+
+console.log(convertToNumber("123"));     
+console.log(convertToNumber("0.123"));   
+console.log(convertToNumber("abc"));      
