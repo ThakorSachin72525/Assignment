4390e825b197037e8a88a31c5a9dd6cc2b4980e0
Sachin Thakor <sachinthakor722446@gmail.com>
2023-07-13 23:27:12 +0530

    V1

---
+++ JS/Error Handling and Prototypes Assignment - 13/1. Type Conversion - Copy - Copy/script.js
@@ -0,0 +1,15 @@
+function convertToNumber(str) {
+    try {
+        const num = Number(str);
+        if (isNaN(num)) {
+            throw new Error("Invalid number");
+        }
+        return num;
+    } catch (error) {
+        return "Invalid number";
+    }
+}
+
+console.log(convertToNumber("123"));     
+console.log(convertToNumber("0.123"));   
+console.log(convertToNumber("abc"));      
