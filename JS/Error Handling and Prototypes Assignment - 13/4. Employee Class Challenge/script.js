3ed51510a2ec727396aed114bc002fa924d9cd93
Sachin Thakor <sachinthakor722446@gmail.com>
2023-07-16 00:04:01 +0530

    4. Emp Class Challenge Uploaded

--- JS/Error Handling and Prototypes Assignment - 13/4. Employee Class Challenge/script.js
+++ JS/Error Handling and Prototypes Assignment - 13/4. Employee Class Challenge/script.js
@@ -1,3 +1,15 @@
-let a = 5
-let b = 6
-console.log(a + b);
+class emp {
+    constructor(name, position, salary){
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    getSalary(){
+        return `The salary of ${this.name} is ${this.salary}`;
+    }
+}
+
+const employee1 = new emp("Sachin", "SME", 35000);
+const getsalary = employee1.getSalary();
+console.log(getsalary);
